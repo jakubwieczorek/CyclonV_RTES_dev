d746ee9e71c0a8c6443a5d6a7d37410d1bc95d20
jakub <jakub.wieczorek0101@gmail.com>
2020-05-01 14:00:42 +0200

    gmon profiling done 2 times for accelerator

--- custom_instruction_profiling/hw/quartus/software/bit_operations3/src/main.c
+++ custom_instruction_profiling/hw/quartus/software/bit_operations3/src/main.c
@@ -21,22 +21,22 @@
 
 #define SLEEP_DELAY_US (100 * 1000)
 
-#define size 1000000
+#define size 2000000
 unsigned int data[size], result[size];
 
 int main() {
-	printf("DE1-SoC nios demo\n");
+	//printf("DE1-SoC nios demo\n");
 
-	setup_leds();
+	//setup_leds();
 
 	for(int i = 0; i < size; i++) {
 		IOWR_32DIRECT(&data[i], 0, 0x12345678);
 		IOWR_32DIRECT(&result[i], 0, 0x00000000);
 		//IOWR_32DIRECT(&result[i], 0, 0x78563412);
 	}
-	printf("A\n");
+	//printf("A\n");
 	bit_reshuffle_hardware_accelerator(&data[0], &result[0], size);
-	printf("B\n");
+	//printf("B\n");
 //	for(int i = 0; i < size; i++){
 //		printf("result      =0x%x, 0x%x\n",   &result[i], IORD_32DIRECT(&result[i], 0));
 //	}
@@ -47,6 +47,7 @@ int main() {
 //		usleep(SLEEP_DELAY_US);
 //	}
 
-	printf("DE1-SoC nios done\n");
-	//exit(EXIT_SUCCESS);
+	//printf("DE1-SoC nios done\n");
+	exit(EXIT_SUCCESS);
+	//return 0;
 }
